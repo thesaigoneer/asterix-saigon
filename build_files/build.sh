fa92a079202f15f1accb964510aacccf127e3995
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-03-28 07:54:25 +0700

    Update build.sh - tryout 1 to remove packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,11 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
+dnf5 remove -y input-remapper
+dnf5 remove -y kde-connect
+dnf5 remove -y kdeconnectd
+dnf5 remove -y kmahjongg
 
 # Use a COPR Example:
 #
